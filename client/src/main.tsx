2f99f1183a820cb4293bed94fb1c2f33db650b7a
ayooladopewilly <46218844-ayooladopewilly@users.noreply.replit.com>
2025-11-12 14:17:14 +0000

    Restored to '227d0ae7659bc91c7f01ba8433d672e0da57f1ed'
    Replit-Restored-To: 227d0ae7659bc91c7f01ba8433d672e0da57f1ed

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,12 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { Buffer } from "buffer";
 import App from "./App";
 import "./index.css";
 import "./lib/i18n";
-
-// Polyfill Buffer for browser (required for Solana/crypto modules)
-globalThis.Buffer = Buffer;
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
